23693b235a99c720b62ca2fb1c9541a302156255
Igor Vieira <igorvieira.inatel@gmail.com>
2024-02-21 11:54:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,77 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class Login extends AppCompatActivity {
+
+    MqttHelper mqttHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mqttHelper = new MqttHelper(getApplicationContext());
+
+        startMqtt();
+
+        EditText UserEditText = findViewById(R.id.user);
+        EditText SenhaEditText = findViewById(R.id.senha);
+        Button BotaoLogin = findViewById(R.id.loginbtn);
+
+        BotaoLogin.setOnClickListener(view -> {
+            mqttHelper.publish(UserEditText.getText().toString()+"/"+SenhaEditText.getText().toString(),
+                    "MeuNovoApp/"+mqttHelper.getClientId()+"/Login/Infos");
+        });
+
+    }
+
+    private void startMqtt() {
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+            }
+            @Override
+            public void connectionLost(Throwable throwable) {
+                //Aparece essa mensagem sempre que a conexão for perdida
+                Toast.makeText(getApplicationContext(), "Conexão perdida", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            // messageArrived é uma função que é chamada toda vez que o cliente MQTT recebe uma mensagem
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                if(topic.equals("MeuNovoApp/"+mqttHelper.getClientId()+"/Login/Infos")) {
+
+                    // Altere os valores necessários dentro da condição "if" para validar o usuário e senha
+                    try {
+                        String[] strings = mqttMessage.toString().split("/");
+                        if (strings[0].equals("Igor") && strings[1].equals("abc")) {
+                            Toast.makeText(Login.this, "Login aprovado!", Toast.LENGTH_SHORT).show();
+                        } else if (strings[0].equals("admin") && !strings[1].equals("admin")) {
+                            Toast.makeText(Login.this, "Senha incorreta!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login.this, "Credenciais inválidas!", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (Error e) {
+                        Toast.makeText(Login.this, "Erro: " + e, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MqttHelper.java
@@ -0,0 +1,137 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.DisconnectedBufferOptions;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+
+//Informações do Broker
+public class MqttHelper {
+    private MqttAndroidClient mqttAndroidClient;
+    final private String serverUri = "tcp://192.168.137.1:1883";
+    final public String clientId = MqttClient.generateClientId();
+    private String activity;
+
+    public MqttHelper(Context context) {
+        activity = context.getClass().getSimpleName();
+
+        mqttAndroidClient = new MqttAndroidClient(context, serverUri, clientId);
+        mqttAndroidClient.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Log.w("mqtt", s);
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) {
+                Log.w("Mqtt", mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+
+
+        });
+        connect();
+    }
+
+    public void setCallback(MqttCallbackExtended callback) {
+        mqttAndroidClient.setCallback(callback);
+    }
+
+    private void connect() {
+        MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+        mqttConnectOptions.setAutomaticReconnect(true);
+        mqttConnectOptions.setCleanSession(false);
+
+        //User e Senha de conexão com o Broker
+        mqttConnectOptions.setUserName("Igor");
+        mqttConnectOptions.setPassword("abc".toCharArray());
+
+        try {
+
+            mqttAndroidClient.connect(mqttConnectOptions, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+
+                    DisconnectedBufferOptions disconnectedBufferOptions = new DisconnectedBufferOptions();
+                    disconnectedBufferOptions.setBufferEnabled(true);
+                    disconnectedBufferOptions.setBufferSize(100);
+                    disconnectedBufferOptions.setPersistBuffer(false);
+                    disconnectedBufferOptions.setDeleteOldestMessages(false);
+                    mqttAndroidClient.setBufferOpts(disconnectedBufferOptions);
+                    subscribeToTopic("MeuNovoApp/"+clientId+"/#");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("Mqtt", "Failed to connect to: " + serverUri + exception.toString());
+                }
+            });
+
+
+        } catch (MqttException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    //Entrando no tópico
+    public void subscribeToTopic(String topic) {
+        try {
+            mqttAndroidClient.subscribe(topic, 0, null, new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.w("Mqtt", "Subscribed!");
+                    if (activity.equals("MainActivity")) {
+                        publish("Conectado ao tópico!", topic.replaceAll("#", activity+"/ConnectionStatus"));
+                    }
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.w("Mqtt", "Subscribed fail!");
+                }
+            });
+
+        } catch (MqttException ex) {
+            System.err.println("Exceptionst subscribing");
+            ex.printStackTrace();
+        }
+    }
+
+    //Bloco que envia comandos para o broker
+    public void publish(String payload, String topic) {
+        byte[] encodedPayload = new byte[0];
+        //teste de conexão
+        try {
+            encodedPayload = payload.getBytes("UTF-8");
+            MqttMessage message = new MqttMessage(encodedPayload);
+            mqttAndroidClient.publish(topic, message);
+        } catch (UnsupportedEncodingException | MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getClientId(){
+        return clientId;
+    }
+}
